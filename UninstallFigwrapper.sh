f303c47746ebf8f25c65d66914bb3baeda698c2a
Cristiano Vitorino <57003922+cristianovitorino@users.noreply.github.com>
2020-04-15 23:05:48 -0300

    Improve uninstall script

--- UninstallFigwrapper.sh
+++ UninstallFigwrapper.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
+echo ""
 echo "Thanks for using Figwrapper!"
-echo "You need to be root to execute this action"
+echo ""
+sleep 1
+
 sudo rm -rf $HOME/Applications/Figma/
 echo "Deleting main folder..."
+echo ""
+sleep 1
+echo "Done!"
+echo ""
 sleep 1
 
 echo "Deleting global executable..."
+echo ""
 sudo rm -rf /usr/local/bin/figma
+sleep 1
+echo "Done!"
+echo ""
 sleep 1
 
 echo "Deleting icon..."
-sudo rm -rf /usr/local/share/icons/figma/figma.png
+echo ""
+sudo rm -rf /usr/local/share/icons/figma/
+sleep 1
+echo "Done!"
+echo ""
 sleep 1
 
 echo "Deleting .desktop..."
+echo ""
 sudo rm -rf $HOME/.local/share/applications/Figma.desktop
 sleep 1
-
 echo "Done!"
+echo ""
+sleep 1
+
+echo "Figwrapper is uninstalled from your system! Thanks for trying it!"
+echo ""
